134be5a898a66039cb811e521601366d5a3951c7
funnymanpatrick <funnymanpatrick@gmail.com>
2013-12-05 16:26:19 -0500

    text start

--- hw8sol.py
+++ hw8sol.py
@@ -54,31 +54,17 @@ if __name__ == "__main__":
         chart_1.after(wait_time)
         for b in balls:
             if b.draw == True:
-                
-                #  Here is the time in milliseconds between consecutive instances
-                #  of drawing the ball.  If this time is too small the ball will
-                #  zip across the canvas in a blur.
-                
-                #  Remove all the previously-drawn balls
-                #chart_1.delete(tk.ALL)
-                
-                # Draw an oval on the canvas within the bounding box
-                bounding_box = (b.x-b.r, b.y-b.r,b.x+b.r, b.y+b.r) 
-                chart_1.create_oval(bounding_box, fill=b.c)
-                chart_1.update()      # Actually refresh the drawing on the canvas.
-        
-                # Pause execution.  This allows the eye to catch up
-        
-        
-                # Move the ball
                 b.move()
-    
                 b.wallcheck(xbound, ybound)
-                
                 for ball in balls:
                     if ball != b:
                         if b.intersect(ball):
                             balls.append(b.combine(ball))
+            if graphics:
+                bounding_box = (b.x-b.r, b.y-b.r,b.x+b.r, b.y+b.r) 
+                chart_1.create_oval(bounding_box, fill=b.c)
+                chart_1.update()
+                
     ## This is an infinite loop that allows the window to listen to
     ## "events", which are user inputs.  The only user event here is
     ## closing the window, which ends the program. 
